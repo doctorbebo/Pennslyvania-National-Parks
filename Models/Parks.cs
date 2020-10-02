44195e9cc4c8c712f5794e536b33647bdb9fecd3
Byron Brown <byronrb92@gmail.com>
2020-10-02 14:46:45 -0400

    Added Models folder and Parks Class

---
+++ Models/Parks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace Pennslyvania_National_Parks.Models
+{
+    public class Parks
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+
+}
